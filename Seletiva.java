33eed4764411527124fb07c37b7530d36d8964ab
Dio Lemos <pedro.devsc@gmail.com>
2024-04-30 10:47:07 -0300

    criar lista de candidatos no main é criado uma arrayList com 20 candidatos e salários aleatórios

--- Seletiva.java
+++ Seletiva.java
@@ -1,5 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Seletiva {
     public static void main(String[] args) {
         
+        private static final double SALARIO_BASE = 2000.0;
+
+        List<Candidato> candidatosList = new ArrayList<>();
+
+        String[] nomes = {"Sócrates", "Platão", "Aristóteles", "Pitágoras", "Heráclito",
+                                    "Demócrito", "Epicuro", "Diógenes", "Zenão", "Tales",
+                                    "Anaximandro", "Empédocles", "Protágoras", "Parmênides", "Anaxágoras",
+                                    "Empédocles", "Demócrito", "Heráclito", "Xenófanes", "Anaxímenes"};
+
+
+
+
+
+
     }
 }
